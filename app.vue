b9fcfced9a4a5d10c470c17883a9f4a9fa584c8c
Anthony Cabigayan <ancabigayan@tesda.gov.ph>
2023-12-27 15:07:56 +0800

    second commit

--- app.vue
+++ app.vue
@@ -1,9 +1,7 @@
 <template>
   <div>
-    <div class="w-full flex items-center">
-      <div class="flex flex-auto truncate items-center">
-        <h1 class="font-normal text-xl md:text-xl">Education</h1>
-      </div>
+    <div class="w-full flex items-center gap-2">
+      <div class="flex flex-auto truncate items-center"></div>
       <div class="ml-auto flex items-center">
         <h1>Home</h1>
       </div>
